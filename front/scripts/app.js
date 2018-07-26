a77a52b3e1d936fd59357a4f7f1fa94b3ef0b6f9
Daniel Golubiewski <dgolubiewski@9bits.pl>
2018-07-26 16:54:34 +0200

    Front - JS, Styles, Views

---
+++ front/scripts/app.js
@@ -0,0 +1,13 @@
+angular.module('cmsDG', [
+  'ngRoute',
+  'ngResource',
+  'ngStorage',
+  'appRoutes',
+  'enterStroke',
+  'MainController',
+  'TodoController',
+  'UserController',
+  'UserService',
+  'TodoService',
+]);
+
