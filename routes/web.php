7bf736f1a0f76e9c2a5897b0e9a639da66df9d24
Daniel Golubiewski <dgolubiewski@9bits.pl>
2018-07-26 16:32:11 +0200

    Deafult Routes

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layout');
+});
+
+// Catch all undefined routes. Always gotta stay at the bottom since order of routes matters.
+Route::any('{undefinedRoute}', function ($undefinedRoute) {
+    return view('layout');
+})->where('undefinedRoute', '([A-z\d-\/_.]+)?');
